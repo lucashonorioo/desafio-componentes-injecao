8672f32a68be21baca8a719868faf7d4d58e3ec6
Lucas Honorio <lucashonorioo@hotmail.com>
2025-06-03 10:47:22 -0300

    Projeto Criado

---
+++ src/main/java/com/estudo/desafio_componentes_injecao/DesafioComponentesInjecaoApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.desafio_componentes_injecao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioComponentesInjecaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioComponentesInjecaoApplication.class, args);
+	}
+
+}
